af8c2acde5f73ee305c7259e79512fe7dabae4a8
Mohammed Emad <31746654+mohammedBadawi@users.noreply.github.com>
2019-11-17 23:37:03 +0200

    Last Update

--- RF.v
+++ RF.v
@@ -5,39 +5,39 @@ input[31:0]writedata;
 input write,clk;
 reg[31:0]rf[0:31];
 output  [31:0] data1,data2, data2_mem; 
-
-
- integer file ;
+integer file ;
+integer i;
 
 initial
 begin
-rf[0] = 10 ;  // rs
+file=$fopen ("C:\\Users\\Mohammed Emad\\Desktop\\MIPS project/Out.txt");
+$fmonitor (file,"%d \n %d\n %d\n %d\n %d",rf[0],rf[1],rf[2],rf[3],rf[4]);
+rf[0] <= 10 ;  // rs
 rf[1] <=20 ;  // rt 
 rf[2]<= 45 ; 
-rf[3]<= 10;
-rf[5]<=6 ; //$zero 
+rf[16]<= 10;
+rf[17]<=6 ; //$zero 
 end
 /////////////////////
-
-
 		
- 		 assign data1 = rf[read1];
- 		 assign data2 = rf[read2];
- 		 assign data2_mem = data2; //for mux after data mem
-
-	
+assign data1 = rf[read1];
+assign data2 = rf[read2];
+assign data2_mem = data2; //for mux after data mem
 
   always@(posedge clk)
    begin
-     if (write)
-     rf[writereg]<=writedata;
+if (write)
+rf[writereg]<=writedata;
+end
 
 
-file = $fopen ("D:\\3rd Computer\\C.O\\Project/Out.txt");
-  $fmonitor (file,"data1 is %d \ndata2 is %d \nresult = %d",data1,data2,writedata);
 
-   
-    end
+
+
+
+
+
+
 /*always @ (rf[2])
 begin
  $monitor ("%d", rf[2]);
