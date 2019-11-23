fc105be91e6c17a02f3203c3bafca5fde2d3ecc4
Mohammed Emad <31746654+mohammedBadawi@users.noreply.github.com>
2019-11-23 21:16:30 +0200

    last update

--- RF.v
+++ RF.v
@@ -3,7 +3,7 @@ input[4:0]read1,read2,writereg;
 input[31:0]writedata;
 input write,clk;
 reg[31:0]rf[0:31];
-output reg[31:0] data1,data2, data2_mem; 
+output [31:0] data1,data2, data2_mem; 
 integer file ;
 integer i;
 
@@ -16,13 +16,17 @@ rf[16]<= 10;
 rf[17]<=6 ; //$zero 
 end
 /////////////////////
-always @(read1,read2)
+assign data1 = rf[read1];
+assign data2 = rf[read2];
+assign data2_mem = data2;
+
+/*always @(read1,read2)
 begin		
  data1 <= rf[read1];
  data2 <= rf[read2];
  data2_mem <= data2; //for mux after data mem
 end
-
+*/
 always@(posedge clk)
 begin
 
